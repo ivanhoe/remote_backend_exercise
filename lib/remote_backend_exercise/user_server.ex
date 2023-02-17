f775da2a3a6520844139549145ac22481bfbbc01
ivan alvarez frias <ivan@resuelve.mx>
2023-02-17 15:47:34 -0600

    Organize first client api methos and after callbacks also:
    - Add constants for better readable code
    - Add helper functions for duplicated code

--- lib/remote_backend_exercise/user_server.ex
+++ lib/remote_backend_exercise/user_server.ex
@@ -4,27 +4,29 @@ defmodule RemoteBackendExercise.UserServer do
   alias RemoteBackendExercise.Context.User
   require Logger
 
+  @max_point_value 101
+  @interval :timer.seconds(60)
+
   def start_link(_args) do
     Logger.debug("Starting user server")
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
-  @impl true
-  def init(_) do
-    :timer.send_interval(10_000, :update_points)
-    {:ok, %{min_number: :rand.uniform(101), timestamp: nil}}
-  end
-
-  @impl true
-  def handle_info(:update_points, state) do
-    task = Task.async(fn -> User.update_all() end)
-    state = Map.put(state, :min_number, :rand.uniform(101))
-    Logger.debug("Nuevo valor de State #{inspect(state)}")
-    {:noreply, state}
-  end
-
   def get_users() do
     GenServer.call(__MODULE__, {:get_users})
+  end
+
+  @impl true
+  def init(_) do
+    update_user_points_every(@interval)
+    {:ok, %{min_number: get_random_number(), timestamp: nil}}
+  end
+
+  @impl true
+  def handle_info({:update_points}, state) do
+    Task.start(fn -> User.update_all() end)
+    update_user_points_every(@interval)
+    {:noreply, Map.put(state, :min_number, get_random_number())}
   end
 
   @impl true
@@ -35,10 +37,23 @@ defmodule RemoteBackendExercise.UserServer do
     {:ok, timestamp} = DateTime.now("Etc/UTC")
     state = Map.put(state, :timestamp, timestamp)
 
-    result = Map.new()
-    result = Map.put(result, :timestamp, DateTime.to_string(timestamp))
-    result = Map.put(result, :users, users)
+    result =
+      Map.new()
+      |> Map.put(:timestamp, DateTime.to_string(timestamp))
+      |> Map.put(:users, users)
 
     {:reply, result, state}
+  end
+
+  # Helper functions
+
+  defp update_user_points_every(interval) do
+    __MODULE__
+    |> Process.whereis()
+    |> Process.send_after({:update_points}, interval)
+  end
+
+  defp get_random_number() do
+    :rand.uniform(@max_point_value)
   end
 end

5e23163ae4d6b4a8047959eeef8b1f5e32e35a9f
ivan alvarez frias <ivan@resuelve.mx>
2023-02-18 21:25:20 -0600

    Improve db population using Multi to execute all the inserts in one transaction

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,8 +10,16 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-Enum.each(1..1_000_000, fn _ ->
-    RemoteBackendExercise.Repo.insert!(%RemoteBackendExercise.User{
-    points: 0
-  })
+alias Ecto.Multi
+alias RemoteBackendExercise.{Repo, User}
+
+1..1_000_000
+|> Enum.reduce(Multi.new(), fn index, multi ->
+  unique_identifier =
+    index
+    |> Integer.to_string()
+    |> String.to_atom()
+
+  Multi.insert(multi, unique_identifier, %User{points: 0})
 end)
+|> Repo.transaction(timeout: :infinity)
